5c11828bec201a94244352ae13b910c9c76adef8
khaled308 <khaledgaballa4@gmail.com>
2023-08-02 14:21:42 +0300

    remote state

--- terraform/03/main.tf
+++ terraform/03/main.tf
@@ -1,3 +1,11 @@
+terraform {
+    backend "s3"{
+        bucket = "khaled-terraform-state"
+        key = "03/terraform.tfstate"
+        region = "us-east-1"
+    }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
