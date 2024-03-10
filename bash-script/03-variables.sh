437e5d958c515fd2bd8d02b0122b2c82ed079241
khaled308 <khaledgaballa4@gmail.com>
2024-03-10 10:47:02 +0200

    shell script

--- bash-script/03-variables.sh
+++ bash-script/03-variables.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-read -p 'Enter Your Name: ' Name
+# read -p 'Enter Your Name: ' Name
 
-echo "Hi $Name"
+# read -p 'Enter Your Full Name: ' FirstName LastName
+
+# read time rest < <(uptime)
+
+# echo "Hi $FirstName $LastName"
